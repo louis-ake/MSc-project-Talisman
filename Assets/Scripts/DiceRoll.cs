1a860979275e5d89ba35d11beb98478bc578c6a9
louis-ake <lakerm01@dcs.bbk.ac.uk>
2018-07-14 13:08:42 +0100

    added basic dice roll script

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DiceValues = new int[1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Results will be stored in public values so they can be accessed from elsewhere
+    public int[] DiceValues;
+    public int DiceTotal;
+
+    public void Roll() {
+        // Will need to be able variable numbers of dice
+        DiceTotal = 0;
+        for (int i = 0; i < DiceValues.Length; i++) {
+            int dieResult = Random.Range(0, 7);
+            DiceTotal += dieResult;
+            DiceValues[i] = dieResult;
+        }
+        Debug.Log("Rolled: " + DiceTotal);
+    }
+}
