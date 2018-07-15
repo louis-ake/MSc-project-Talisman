59eb0b3f9f6dc8138e132f4b0f9ffc943178a6dd
louis-ake <lakerm01@dcs.bbk.ac.uk>
2018-07-15 15:26:50 +0100

    basic TileClick scripts for each region

---
+++ Assets/Scripts/TileClicks/MiddleTileClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleTileClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int Clicks = 0;
+
+    void OnMouseDown()
+    {
+        Clicks += 1;
+        Debug.Log("Clicked on Tile " + this.name + " " + Clicks + " times");
+    }
+}
