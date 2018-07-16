76e81e7d47ca68d86f2554d86699b40654658347
louis-ake <lakerm01@dcs.bbk.ac.uk>
2018-07-16 18:10:55 +0100

    empty tile scipts added

---
+++ Assets/Scripts/TileO1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileO1 : Tile {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void VisitBlacksmithHealerOrMystic()
+	{
+		//TODO
+	}
+}
